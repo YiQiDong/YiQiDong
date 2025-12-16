b0e3c99ca6c6ac791a0dd01e10340ce6c843e68a
张鹏 <scbeta@qq.com>
2025-12-16 22:21:52 +0800

    为容器的功能函数界面新增加的上传功能做好准备

--- src/YiQiDong/Program.cs
+++ src/YiQiDong/Program.cs
@@ -27,6 +27,7 @@ namespace YiQiDong
         internal static void LoadConfig()
         {
             Quick.Protocol.QpAllClients.RegisterUriSchema();
+            Quick.Blazor.Bootstrap.Admin.FileManageControl.GlobalDownloadFileAction = Components.Controls.FileManageControl.HttpDownload;
             Quick.Localize.GettextResourceManager.ChangeCurrentCulture(CultureInfo.GetCultureInfo("zh-CN"));
 #if (!DEBUG)
                 //设置当前目录为程序所在的目录

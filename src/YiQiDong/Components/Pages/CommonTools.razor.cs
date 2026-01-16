c3700bedcfdda20e3c412f0aecfd549cd6cdd1b0
张鹏 <scbeta@qq.com>
2026-01-16 20:32:01 +0800

    实用工具页面整理

--- src/YiQiDong/Components/Pages/CommonTools.razor.cs
+++ src/YiQiDong/Components/Pages/CommonTools.razor.cs
@@ -1,54 +1,87 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Quick.Blazor.Bootstrap;
+using Quick.Blazor.Bootstrap.Utils;
 using YiQiDong.Components.Controls;
-using static YiQiDong.Components.Controls.CommonToolsControl;
 
 namespace YiQiDong.Components.Pages
 {
     public partial class CommonTools : ComponentBase
-    {
+    {        
+        public class ToolInfo
+        {
+            public string Category { get; set; }
+            public string Name { get; set; }
+            public string IconClass { get; set; }
+            public RenderFragment Content { get; set; }
+
+            public static ToolInfo Create<T>(string category, string name, string iconClass, Dictionary<string, object> parameterDict = null)
+            {
+                return new ToolInfo()
+                {
+                    Category = category,
+                    Name = name,
+                    IconClass = iconClass,
+                    Content = BlazorUtils.GetRenderFragment<T>(parameterDict)
+                };
+            }
+        }
+
+        private ModalWindow modalWindow;
         private ToolInfo[] tools;
 
-        protected override void OnParametersSet()
+        public CommonTools()
         {
-            base.OnParametersSet();
-            var toolList = new List<ToolInfo>
-            {
-                ToolInfo.Create<DateTimeManage>("日期时间", "fa fa-clock-o")
-            };
+            tools = GetTools().ToArray();
+        }
+
+        private IEnumerable<ToolInfo> GetTools()
+        {
+            //-----------
+            //系统管理
+            //-----------
+            yield return ToolInfo.Create<DateTimeManage>("系统管理", "日期时间", "fa fa-clock-o");
             if (OperatingSystem.IsLinux())
             {
-                toolList.Add(ToolInfo.Create<CommonTools_LinuxTools>("Linux工具箱", "fa fa-archive"));
+                yield return ToolInfo.Create<LinuxTools.NetworkInterfaceManage>("系统管理", "网卡管理", "fa fa-sitemap");
+                yield return ToolInfo.Create<LinuxTools.FirewallManage_iptables>("系统管理", "防火墙管理[iptables]", "fa fa-shield");
             }
             var hostsFile = "/etc/hosts";
             if (OperatingSystem.IsWindows())
                 hostsFile = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Windows)}\System32\drivers\etc\hosts";
-            toolList.Add(ToolInfo.Create<Quick.Blazor.Bootstrap.Admin.TextEditControl>("Hosts文件", "fa fa-file-text", new()
+            yield return ToolInfo.Create<Quick.Blazor.Bootstrap.Admin.TextEditControl>("系统管理", "Hosts文件", "fa fa-file-text", new()
             {
                 [nameof(Quick.Blazor.Bootstrap.Admin.TextEditControl.File)] = hostsFile
-            }));
-            toolList.AddRange(
-            [
-                ToolInfo.Create<FileManageControl>("文件管理","fa fa-folder"),
-                ToolInfo.Create<Quick.Blazor.Bootstrap.Admin.ProcessManageControl>("进程管理","fa fa-cogs"),
-                ToolInfo.Create<Quick.Blazor.Bootstrap.Terminal.TerminalControl>("模拟终端","fa fa-terminal",new Dictionary<string, object>()
-                {
-                    [nameof(Quick.Blazor.Bootstrap.Terminal.TerminalControl.WorkingDir)]=Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
-                }),
-                ToolInfo.Create<Quick.Blazor.Bootstrap.Admin.ProxyDownloadControl>("代理下载","fa fa-cloud-download"),
-                ToolInfo.Create<CommonTools_TestTools>("测试工具箱", "fa fa-archive"),
-                ToolInfo.Create<CommonTools_DevTools>("开发工具箱", "fa fa-archive")
-            ]);
+            });
+            yield return ToolInfo.Create<FileManageControl>("系统管理", "文件管理", "fa fa-folder");
+            yield return ToolInfo.Create<Quick.Blazor.Bootstrap.Admin.ProcessManageControl>("系统管理", "进程管理", "fa fa-cogs");
+            yield return ToolInfo.Create<Quick.Blazor.Bootstrap.Terminal.TerminalControl>("系统管理", "模拟终端", "fa fa-terminal", new Dictionary<string, object>()
+            {
+                [nameof(Quick.Blazor.Bootstrap.Terminal.TerminalControl.WorkingDir)] = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+            });
             if (Program.IsStartSuccess)
             {
-                toolList.AddRange(new[]
-                {
-                    ToolInfo.Create<Quick.Blazor.Bootstrap.ReverseProxy.ReverseProxyManageControl>("反向代理", "fa fa-paper-plane"),
-                    ToolInfo.Create<CommonTools_Glash>("Glash代理", "fa fa-paper-plane"),
-                    ToolInfo.Create<Quick.Blazor.Bootstrap.CrontabManager.CrontabManageControl>("Crontab管理", "fa fa-align-justify")
-                });
+                    yield return  ToolInfo.Create<Quick.Blazor.Bootstrap.CrontabManager.CrontabManageControl>("系统管理",  "Crontab管理", "fa fa-align-justify");
             }
-            tools = toolList.ToArray();
+            //-----------
+            //代理工具
+            //-----------
+            yield return ToolInfo.Create<GlashServerControl>("代理工具", "Glash服务端","fa fa-server");
+            yield return ToolInfo.Create<GlashAgentControl>("代理工具", "Glash代理端","fa fa-black-tie");
+            yield return ToolInfo.Create<Glash.Blazor.Client.Main>("代理工具", "Glash客户端","fa fa-coffee");
+            yield return ToolInfo.Create<Quick.Blazor.Bootstrap.ReverseProxy.ReverseProxyManageControl>("代理工具", "反向代理", "fa fa-paper-plane");
+            yield return ToolInfo.Create<Quick.Blazor.Bootstrap.Admin.ProxyDownloadControl>("代理工具", "代理下载","fa fa-cloud-download");
+            //-----------
+            //其他工具
+            //-----------
+            yield return ToolInfo.Create<TestTools.PingTestControl>("其他工具","Ping测试","fa fa-bolt");
+            yield return  ToolInfo.Create<TestTools.TcpTestControl>("其他工具","TCP测试","fa fa-bolt");
+            yield return ToolInfo.Create<DevTools.RasKeyPairGenerator>("其他工具", "RSA密钥对生成", "fa fa-bolt");
+            yield return ToolInfo.Create<DevTools.GuidGenerator>("其他工具", "GUID生成", "fa fa-bolt");           
+        }
+
+        public void Show(ToolInfo toolInfo)
+        {
+            modalWindow.Show(toolInfo.Name, toolInfo.Content);
         }
     }
 }

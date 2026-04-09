e3f3aa9302c284cf5e8e3f9a54608cfccac400db
张鹏 <scbeta@qq.com>
2026-04-09 12:08:02 +0800

    发布版本：2026.25.409

--- src/YiQiDong/Consts.cs
+++ src/YiQiDong/Consts.cs
@@ -2,7 +2,7 @@
 
 public class Consts
 {
-    public const string Version = "2026.24.403";
+    public const string Version = "2026.25.409";
     public const string ARCH = "any";
 
     public const string CONFIG_JSON_FILENAME = "config.json";

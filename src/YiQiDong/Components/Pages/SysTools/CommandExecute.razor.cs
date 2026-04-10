a843c853bf907f1384bc2066c80b040f88ef968f
张鹏 <scbeta@qq.com>
2026-04-10 12:10:33 +0800

    初步完成命令执行功能

--- src/YiQiDong/Components/Pages/SysTools/CommandExecute.razor.cs
+++ src/YiQiDong/Components/Pages/SysTools/CommandExecute.razor.cs
@@ -3,56 +3,41 @@ using Quick.Utils;
 
 namespace YiQiDong.Components.Pages.SysTools;
 
-public partial class CommandExecute : IDisposable
+public partial class CommandExecute
 {
     public int ConsoleRows = 20;
     private string command;
-    private Quick.Shell.ICommandContext commandContext;
     private LogViewControl logViewControl;
-
-    protected override void OnInitialized()
-    {
-        if (OperatingSystem.IsWindows())
-        {
-            commandContext = new Quick.Shell.WinCmd.WinCmdCommandContext();
-        }
-        else
-        {
-            commandContext = new Quick.Shell.UnixShell.UnixShellCommandContext();
-        }
-    }
-
-    public void Dispose()
-    {
-        commandContext?.Dispose();
-    }
 
     private void ConsoleSetRows(int rows)
     {
         ConsoleRows = rows;
     }
 
-    private async Task Ok()
+    private void Ok()
     {
         var currentCommand = command;
         command = null;
-        await Task.Delay(10);
-        if (!string.IsNullOrEmpty(currentCommand))
+        Task.Run(() =>
         {
-            logViewControl.AddLine($"> {currentCommand}");
+            if (!string.IsNullOrEmpty(currentCommand))
+            {
+                logViewControl.AddLine($"> {currentCommand}");
 
-            try
-            {
-                var ret = commandContext.ExecuteCommand(currentCommand, false);
-                if (ret.Output != null)
-                    foreach (var line in ret.Output)
-                        logViewControl.AddLine(line);
+                try
+                {
+                    var ret = Quick.Shell.Utils.ProcessUtils.ExecuteShell(currentCommand);
+                    if (!string.IsNullOrEmpty(ret.Output))
+                        logViewControl.AddLine(ret.Output);
+                    if (!string.IsNullOrEmpty(ret.Error))
+                        logViewControl.AddLine(ret.Error);
+                }
+                catch (Exception ex)
+                {
+                    logViewControl.AddLine(ExceptionUtils.GetExceptionString(ex));
+                }
             }
-            catch (Exception ex)
-            {
-                logViewControl.AddLine(ExceptionUtils.GetExceptionString(ex));
-            }
-        }
-        logViewControl.AddLine(">");
+            logViewControl.AddLine(">");
+        });
     }
 }

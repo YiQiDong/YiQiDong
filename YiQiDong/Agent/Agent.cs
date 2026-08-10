3d29cc57545de4cfbc21a783be14ce4151494d82
张鹏 <scbeta@qq.com>
2026-08-10 15:23:15 +0800

    支持部分老版本镜像

--- YiQiDong/Agent/Agent.cs
+++ YiQiDong/Agent/Agent.cs
@@ -11,7 +11,7 @@ namespace YiQiDong.Agent
         {
             var agentTypeName = AgentContext.Container.Image.AgentType;
             if (agentTypeName == null)
-                throw new IOException($"镜像未配置[{nameof(AgentContext.Container.Image.AgentExecute)}]或[{nameof(AgentContext.Container.Image.AgentType)}]");
+                agentTypeName = nameof(AgentTypes.AppSettings);
             switch (agentTypeName)
             {
                 case nameof(AgentTypes.TextConfigs):

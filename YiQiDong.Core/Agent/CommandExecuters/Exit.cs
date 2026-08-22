c5afb8e9b12671abd4a5d57c6a05ad663e345b5c
张鹏 <scbeta@qq.com>
2026-08-22 11:05:21 +0800

    小优化

--- YiQiDong.Core/Agent/CommandExecuters/Exit.cs
+++ YiQiDong.Core/Agent/CommandExecuters/Exit.cs
@@ -16,7 +16,7 @@ namespace YiQiDong.Agent.CommandExecuters
                 AgentContext.LogError($"停止容器上下文时出错，原因：{ExceptionUtils.GetExceptionString(ex)}");
             }
             //等待1秒，退出容器进程
-            Task.Delay(1000).ContinueWith(t => AgentContext.Dispose());
+            _ = Task.Delay(1000).ContinueWith(t => AgentContext.Dispose());
             return new Protocol.V1.QpCommands.Exit.Response();
         }
     }
